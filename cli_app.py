057f60989c97baeef7fb17de6ad32ab84844efdd
lberr8 <lhberridge@yahoo.co.uk>
2025-04-13 16:58:52 +0100

    Added some functions: create_tables initialise_table_data
    wrote implementation plan for  cli menu options.

--- cli_app.py
+++ cli_app.py
@@ -57,3 +57,53 @@ def initial_menu_loop(choice):
 
 choice = print_menu()
 initial_menu_loop(choice)
+
+def add_new_flight():
+    # ask for each attribute
+    # check correct data type had been entered
+    # insert record into database
+    # display full record when data entry has been completed using select statement to prove it has been entered (WHERE flight_no = x)
+
+    pass
+
+def view_flights_by_criteria():
+    # display menu of criteria to add
+    # add criteria to select statment
+    # there could be a varying amount of criteria, so maybe add criteria to list, then use this list to populate select statement?
+    # display result of select statment
+    pass
+
+def update_flight_info():
+    # ask which flight and present list of possible flight_no
+    # present menu of what the person can change
+    # allow one thing to be changed at once
+    # go back to menu - do you want to change anything else? Y continue in this menu
+    # if no, do you want to change info on another flight? Y - Present list of flight nos again
+    # N - go back to main menu
+    # at end display updated record
+    pass
+
+def assign_pilot_to_flight():
+    # ask which pilot from presented list of pilots in table
+    # ask which flight from presented list of flights in table
+    # check pilot is available
+    # if available, add pilot to flight and print update record
+    # if not say pilot cannot be assigned and ask if they want to assign another pilot
+    pass
+
+def view_pilot_schedule():
+     # ask which pilot from presented list of pilots in table
+     # display pilot_id, flight_no, flight_dest, departure_date and arrival_date and times
+    pass
+
+def view_destination_info():
+    # ask which destination from list of valid desitination ids or if want all to be displayed
+    pass
+
+def update_destination_info():
+    # ask which destination from list
+    # add destination as an option?
+    # present menu of destination fields that can be modified
+    # check type
+    # similar flow to update flight above
+    pass
